f0d7f9dd1dd12950b0d899589f6bf615a5697df5
Hélio Rugani Brandão <hrugani@gmail.com>
2017-07-08 17:14:33 -0300

    Update PersistenceInterface.java

--- src/com/mycompany/myapp/PersistenceInterface.java
+++ src/com/mycompany/myapp/PersistenceInterface.java
@@ -10,6 +10,25 @@ import java.util.Collection;
 /**
  *
  * @author hrugani
+ *
+ * Interface that abstracts all interactions that The App needs
+ * to execute its persistence behavior.
+ *
+ * It acts as a contract that all persistence classes must implements.
+ *
+ * This project offers 2 different persistence classes as example.
+ * Memory persistence and local storage persistence.
+ * This interface isolates persistences tasks from visual layer.
+ * Using this approach we can change the persistence implemetation
+ * without change anything  in visual layer.
+ *
+ * As you can see, this interface defines one method for each basic CRUD operation:
+ * Create ( addClient), Read (getClients), Update (updateClient) and Delete (removeClient).
+ *
+ * One method "getNextId" was added only for delegating ID generation
+ * for new objects. In this way the persistence layer can decide
+  * to use a strategy that fits better in each case.  
+ * 
  */
 public interface PersistenceInterface {
 
